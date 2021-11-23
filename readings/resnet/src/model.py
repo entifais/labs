fab00ccc2b9240c32968dc367a64cb1702aa7261
jero98772 <jero98772@protonmail.com>
2021-11-23 17:18:54 -0500

    udpate model

--- readings/resnet/src/model.py
+++ readings/resnet/src/model.py
@@ -39,8 +39,29 @@ class BottleneckBlock(nn.Module):
     def __init__(self):
         super().__init__()
 
+        self.net = nn.ModuleList([
+            self.make_block(in_channels, out_channels),
+            self.make_block(out_channels, out_channels)
+        ])
+        projection = nn.ModuleList([
+            self.make_projections(in_channels, out_channels),
+            self.make_projections(out_channels, out_channels)
+        ])
+        net=nn.Sequential(
+        )
+    def make_projections(in_channels, out_channels):
+        if in_channels==out_channels:
+            projection=nn.Identity()
+        else:
+            projection=nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=1)
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
-        pass
+        latent = input_tensor
+
+        for func in self.net:
+            latent = func(latent) + latent
+        
+        return latent
+
 
 class Resnet(nn.Module):
     def __init__(self):
