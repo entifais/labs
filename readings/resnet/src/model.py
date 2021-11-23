1fb7c21dc9f68be0482abd63141ac1ab430a1f4b
santiago <shpotes3@gmail.com>
2021-11-23 15:09:06 -0500

    feat: add residual block

--- readings/resnet/src/model.py
+++ readings/resnet/src/model.py
@@ -3,11 +3,37 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class ResidualBlock(nn.Module):
-    def __init__(self):
+    """
+    Resnet building block
+
+    y = F(x, {Wi}) + x.
+    """
+    def __init__(self, in_channels, out_channels):
         super().__init__()
 
+        self.net = nn.ModuleList([
+            self.make_block(in_channels, out_channels),
+            self.make_block(out_channels, out_channels)
+        ])
+        
+    def make_block(self, in_channels, out_channels):
+        net = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+        )
+        
+        return net 
+
     def forward(self, input_tensor: torch.Tensor) -> torch.Tensor:
-        pass
+        latent = input_tensor
+
+        for func in self.net:
+            latent = func(latent) + latent
+        
+        return latent
 
 class BottleneckBlock(nn.Module):
     def __init__(self):
