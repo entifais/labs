625bc15d97cf6c608a281ea07090ad033500caf8
santiago <shpotes3@gmail.com>
2021-11-22 19:25:03 -0500

    improve skeleton

--- readings/resnet/src/data.py
+++ readings/resnet/src/data.py
@@ -1,5 +1,23 @@
+import pytorch_lightning as pl
+from torch.utils import data
 from torchvision.datasets import CIFAR10
 from torchvision import transforms as T
 
 # CIFAR_MEAN = 
 # CIFAR_STD = 
+
+class CifarDataModule(pl.LightningDataModule):
+    def __init__(self):
+        super().__init__()
+
+    def prepare_data(self):
+        pass
+
+    def setup(self):
+        pass
+
+    def train_dataloader(self) -> data.DataLoader:
+        pass
+
+    def val_dataloader(self) -> data.DataLoader:
+        pass
--- readings/resnet/src/model.py
+++ readings/resnet/src/model.py
@@ -1,3 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+class ResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_tensor):
+        pass
+
+class BottleneckBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_tensor):
+        pass
+
+class ResNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.init_weights()
+
+    def init_weights(self):
+        for name, module in self.named_modules():
+            pass # init modules
+
+    def forward(self, input_tensor):
+        pass
